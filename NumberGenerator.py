5a572cfe3bd59cabd833b17d54e6b9a077258f02
Christopher Fisher Jr <chrisfisher419@gmail.com>
2021-02-21 16:40:20 -0600

    Initial Commit

---
+++ NumberGenerator.py
@@ -0,0 +1,9 @@
+import random
+
+print("Random Number Generator!")
+count = input("How many numbers would you like to generate?")
+count = int(count)
+for p in range(count):
+    number = random
+    print(random.randint(random.randint(0,100), random.randint(101,1000)))
+print("Enjoy your numbers!")
